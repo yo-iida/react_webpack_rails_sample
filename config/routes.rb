8a1ccd02b9b3645e8e9dd08b6009f7084c5d16b0
飯田 意己 <lohocla@icloud.com>
2016-03-12 22:50:19 +0900

    react_webpack_rails:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'react_examples/component', to: 'react_examples#component', as: :component
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
